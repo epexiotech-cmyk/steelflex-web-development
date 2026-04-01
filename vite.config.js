0fc25abba40a10ec93c5d10bb8275aca3390cc04
Apurv Patel <95409482+apurv9112@users.noreply.github.com>
2026-04-01 16:01:14 +0530

    add build files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/steelflex-web-development/',
     root: '.',
     publicDir: 'public',
     build: {
